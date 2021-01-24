4300a197553f7f72c14ba873371f04ef3411039a
Leguu <33807061+Leguu@users.noreply.github.com>
2021-01-23 20:19:34 -0500

    Fix copyright

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-// Asil Erturan (40164714) and Christian Jerjian (40031909)
-// COMP249
-// Assignment #1
-// 2020-02-05
+/**
+ * Asil Erturan (40164714) and Christian Jerjian (40031909)
+ * COMP249
+ * Assignment #1
+ * 2020-02-05
+ */
 
 public class Main {
 
