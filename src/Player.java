b72384efe98b5b5538a1b6d23428f27eb4c3106f
Leguu <33807061+Leguu@users.noreply.github.com>
2021-01-29 17:05:20 -0500

    Add getters and setters

--- src/Player.java
+++ src/Player.java
@@ -6,8 +6,24 @@
  */
 
 public class Player {
-    public int position;
-    public String name;
+    private int position;
+    private String name;
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Player(int position, String name) {
         this.position = position;
