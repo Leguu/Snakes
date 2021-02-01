221bf1abb88696ea822b1d93f49bb9505157b175
Leguu <33807061+Leguu@users.noreply.github.com>
2021-02-01 12:06:50 -0500

    Modifications to player count input

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@
  * 2020-02-05
  */
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -19,11 +18,19 @@ public class Main {
         // Ask for player count
         int playerCount = 0;
 
-//        ------------------------ Need to stop after 4 attempt, look at part 2. i think the best way to do this is to use for loop ----------------------------
-        while (playerCount == 0) {
+        for (int attempts = 0; playerCount < 2 || 4 < playerCount; attempts += 1) {
+            // Why is this check a thing? Useless
+            if (attempts == 4) {
+                System.out.println("Failed to enter player count 4 times. Exiting...");
+                return;
+            }
+
+            Driver.promptUser("How many players are you");
+
             try {
-                Driver.promptUser("How many players are you");
-                playerCount = Integer.parseInt(s.next());
+                playerCount = Integer.parseInt(s.nextLine());
+                if (playerCount < 2 || 4 < playerCount)
+                    System.out.println("Please enter a number between 2 and 4.");
             } catch (NumberFormatException e) {
                 System.out.println("Error: Please input a number");
             }
@@ -32,8 +39,6 @@ public class Main {
         Driver game = new Driver(playerCount);
         game.play();
 
-
         System.out.println("--- Program Terminated ---");
-        s.close();
     }
 }
