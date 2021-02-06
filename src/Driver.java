dd594f3994aa748093f42d1c38cdad1cea3b70b8
Christian Jerjian <christianjerjian@gmail.com>
2021-02-06 05:55:50 -0500

    both bugs have been fixed.
    Javadocs done

--- src/Driver.java
+++ src/Driver.java
@@ -10,6 +10,14 @@ import java.util.Comparator;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ *
+ * GameEnd class which assigns the winning player to a new player calling winner.
+ *
+ * @author Asil Erturan (40164714) and Christian Jerjian (40031909)
+ * @version 1
+ *
+ */
 class GameEnd extends Exception {
     public Player winner;
 
@@ -18,10 +26,22 @@ class GameEnd extends Exception {
     }
 }
 
+/**
+ *
+ * Driver class containing a board and a the player array.
+ *
+ * @author Asil Erturan (40164714) and Christian Jerjian (40031909)
+ * @version 1
+ *
+ */
 public class Driver {
     private final Board board = new Board();
     private final Player[] players;
 
+    /**
+     * A parameterized constructor of the driver class. It creates an array of players and initializes player objects to this array.
+     * @param players The number of participating players
+     */
     public Driver(int players) {
         this.players = new Player[players];
 
@@ -40,17 +60,28 @@ public class Driver {
         setOrder();
     }
 
+    /**
+     * This method flips the dice and returns an integer value.
+     * @return A value between 1 to 6 to represent the coin flip
+     */
     public static int flipDice() {
         Random random = new Random();
         return random.nextInt(6) + 1;
     }
 
+    /**
+     * This method prompts the user to input a string.
+     * @param prompt The value inputed by user
+     */
     public static void promptUser(String prompt) {
         System.out.println(prompt + ": ");
         System.out.print("> ");
     }
 
-    // Make a user order, and so on.
+    /**
+     * This method makes a user order based on their flip dice. If multiple users get the same dice number, the solveTie() method will be triggered. The method also prints out the order of players.
+     *
+     */
     private void setOrder() {
         System.out.println("Now deciding which player will start playing...");
 
@@ -81,6 +112,11 @@ public class Driver {
     // Should be used in a while loop.
     // Solves the first tie it founds, and returns true if there may be more,
     // or false if the array has no ties.
+
+    /**
+     * This method solves the first tie it finds, and returns true if there are other ties in the setOrder().
+     * @return A boolean value is returned based on wheter the tie has been solved
+     */
     private boolean solveTies() {
         for (int i = 0; i < players.length - 1; i++) {
             Player current = players[i];
@@ -93,6 +129,12 @@ public class Driver {
         return false;
     }
 
+
+    /**
+     * This method solves the following ties after the first one and solves ties between the current and the preceding player. This method also sorts the player array based on each player's order.
+     * @param current A player object which represents the current player
+     * @param next  A player object representing the preceding player
+     */
     private void solveTie(Player current, Player next) {
         System.out.printf("A tie was achieved between %s and %s. Re-rolling...\n", current.name, next.name);
 
@@ -108,6 +150,11 @@ public class Driver {
         Arrays.sort(players, Comparator.comparing(p -> -p.order));
     }
 
+
+    /**
+     * This method does 1 round of flip dice to each players, move the players to the new positions while taking into considerations snakes and ladders.
+     * @throws GameEnd ?????????????????????????????????????????????????????????????
+     */
     private void doRound() throws GameEnd {
         for (Player player : players) {
             int diceNumber = flipDice();
@@ -139,6 +186,9 @@ public class Driver {
         }
     }
 
+    /**
+     * This method asks for an input from the users and gives the choice between displaying the board, roll for the next turn, or quit the program.
+     */
     public void play() {
         Scanner s = new Scanner(System.in);
         String input = "";
